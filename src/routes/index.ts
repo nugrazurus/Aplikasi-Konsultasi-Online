a79456f0274a344bfbc7085d2d50f810609db8b4
nugrazurus <nugrazurus@gmail.com>
2021-11-30 17:16:38 +0700

    buat beberapa view (unresponsive)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,17 @@
 import { Router, Request, Response } from 'express';
-import * as controller from '../controllers/indexController';
+// import * as controller from '../controllers/indexController';
 import * as authController from '../controllers/authController';
 const index = Router();
 
-index.get('/', controller.index);
+index.get('/', (req: Request, res: Response) => {
+  res.status(200).render('index');
+});
+index.get('/logbook', (req: Request, res: Response) => {
+  res.status(200).render('logbook');
+});
+index.get('/logbook/detail', (req: Request, res: Response) => {
+  res.status(200).render('logbook-detail');
+});
 index.post('/login', authController.login);
 index.use('*', (req: Request, res: Response) => {
   res.status(404).render('_404');
