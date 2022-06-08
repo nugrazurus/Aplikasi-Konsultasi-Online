f7b990a3c5ad5a4aed559dfaba1765fd39492c57
nugrazurus <nugrazurus@student.untan.ac.id>
2022-06-08 16:38:18 +0700

    add api calendar

--- src/service/googleCalendar.ts
+++ src/service/googleCalendar.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { google } from 'googleapis';
 
 const calendar = google.calendar('v3');
@@ -59,4 +60,25 @@ export const createEvent = async (payload: Event) => {
       };
     },
   );
+};
+
+export const createEventCalendarGCR = async (payload: any) => {
+  return await axios
+    .post(`${process.env.GCR_API}/kalender/buat-event`, payload)
+    .then((res) => (res.data.gagal ? '' : res.data.data?.hangoutLink))
+    .catch((err) => err.response);
+};
+
+export const getEmailDosen = async (nip: string) => {
+  return await axios
+    .get(`${process.env.GCR_API}/kelas/emaildosen/${nip}`)
+    .then((res) => (res.data.email_dosen === 'null' ? '' : res.data.email_dosen))
+    .catch((err) => err.response);
+};
+
+export const getEmailMahasiswa = async (nim: string) => {
+  return await axios
+    .get(`${process.env.GCR_API}/kelas/email-mhs-plain/${nim}`)
+    .then((res) => (res.data.gagal ? '' : res.data.mhs.email))
+    .catch((err) => err.response);
 };
